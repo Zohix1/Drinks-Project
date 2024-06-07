a2a646252d7f143da59a37f50d72184c10a19e71
wangzy <sdwzy0530@163.com>
2024-06-07 09:30:21 +0800

    测试

--- drinks-boot/src/main/java/com/drinks/erp/Application.java
+++ drinks-boot/src/main/java/com/drinks/erp/Application.java
@@ -1,12 +1,9 @@
 package com.drinks.erp;
 
-import com.drinks.erp.utils.ComputerInfo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
@@ -17,10 +14,14 @@ import java.io.IOException;
 @EnableScheduling
 public class Application {
     public static void main(String[] args) throws IOException {
+        SpringApplication.run(Application.class, args);
+        System.out.println("启动成功");
+/*
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         Environment environment = context.getBean(Environment.class);
         System.out.println("启动成功，后端服务API地址：http://" + ComputerInfo.getIpAddr() + ":"
                 + environment.getProperty("server.port") + "/drinks-boot/doc.html");
         System.out.println("您还需启动前端服务，启动命令：yarn run serve 或 npm run serve，测试用户：jsh，密码：123456");
+*/
     }
 }
